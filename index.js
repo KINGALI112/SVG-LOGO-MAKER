882e17e0926c5ddbd1c0a9d885be8180f0f21b1f
SYED <syedali05125569@gmail.com>
2023-03-03 15:43:27 -0500

    added the code to my reply
    added the code to my reply

---
+++ index.js
@@ -0,0 +1,8 @@
+
+// JS ESM Modules
+// import test from 'node:test';
+
+// JS Common Syntax
+// const test = require('node:test');
+const lib = require("./lib");
+lib();
---
+++ index.test.js
@@ -0,0 +1,11 @@
+
+// JS ESM Modules
+// import test from 'node:test';
+
+
+// JS Common Syntax
+const test = require('node:test');
+
+const assert = require('assert/strict');
+const lib = require("./lib");
+assert.equal(lib())
